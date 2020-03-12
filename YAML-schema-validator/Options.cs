a170e476d7d0a4869559f5a1e04a67d25d460d04
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-03-13 00:42:10 +0530

    yaml and json options added to options

--- YAML-schema-validator/Options.cs
+++ YAML-schema-validator/Options.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace YAML_schema_validator
 {
     class Options
     {
+        [Option('y', "yaml", Required = true, HelpText = "YAML file to validate")]
+        public string yaml { get; set; }
+
+        [Option('j', "json", Required = true, HelpText = "JSON schema to compare")]
+        public string json { get; set; }
     }
 }
