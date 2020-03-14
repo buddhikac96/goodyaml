576fea931d40d761e5d2cd2dbbfbd075b4d99568
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-03-14 21:51:59 +0530

    yaml file name .yaml extension conflict resolved

--- YAML-schema-validator/YamlFile.cs
+++ YAML-schema-validator/YamlFile.cs
@@ -20,6 +20,12 @@ namespace YAML_schema_validator
 
         public string GetYamlFile(string yamlFileName)
         {
+            if (yamlFileName.Contains(".yaml"))
+            {
+                yamlFileName = yamlFileName.Substring(0, yamlFileName.Length - 5);
+                Console.WriteLine(yamlFileName);
+            }
+
             if(!File.Exists(Path.Combine(CurrentWorkingDirectory, yamlFileName + ".yaml")))
             {
                 throw new FileNotFoundException("Unable to locate " + yamlFileName + ".yaml in the current directory");
